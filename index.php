252eb1f15a59b9d87232c08865cebbf62661519f
vanguard07 <avtansdps@gmail.com>
2018-05-08 17:52:54 +0530

    Changes

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
     <title>Twitter API SEARCH</title>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="search.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" type= "text/css" href="style.css">
 </head>
 <body>
-    <div class="searchbar">
-        <form id="form" action="tweets.php" method="post" target="frame">
-            <input type="text" name="hashtag" placeholder="Enter Keyword" id = "query" onkeyup="checkBox()">
-        </form>
+    <div class="row">
+        <div class="col-sm-4"></div>
+        <div class="searchbar col-sm-4">
+            <form id="form" action="tweets.php" method="post" target="frame">
+                <input type="text" name="hashtag" placeholder="Enter hashtag to search" id = "query" onkeyup="checkBox()">
+            </form>
+        </div>
     </div>
     <div id="content" scrolling="no">
         <iframe name="frame" frameBorder="0" ></iframe>
--- tweets.php
+++ tweets.php
@@ -12,6 +12,7 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 <html lang="en" dir="ltr">
     <head>
         <link rel="stylesheet" type="text/css"  href="tweets.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     </head>
     <body>
         <?php
@@ -20,16 +21,25 @@ use Abraham\TwitterOAuth\TwitterOAuth;
             'search/tweets',
             array("q" => "#"."$_POST[hashtag]", "count" => 10, "include_entities" => "true")
             );
-            foreach ($tweets->statuses as $key => $tweets) { ?>
-                <div class="feed">
-                    <blockquote class="twitter-tweet">
-                        <p>
-                            <?php print_r($tweets->text); ?>
-                        </p>
-                            &mdash; <?php print_r($tweets->user->name); ?>(@<?php print_r($tweets->user->screen_name);?>)
-                            <a href="<?php print_r($tweets->entities->urls[0]->url) ?>"><?php print_r(date('jS F, Y', strtotime($tweets->created_at))); ?></a>
-                    </blockquote>
-                </div>
-        <?php } ?>
+        $count = 0;
+            foreach ($tweets->statuses as $key => $tweets) {
+                if ($count%3 == 0) {
+                    ?>
+                    <div class="row">
+            <?php    } ?>
+            <div class="feed col-sm-4">
+                <blockquote class="twitter-tweet">
+                    <p>
+                        <?php print_r($tweets->text); ?>
+                    </p>
+                        &mdash; <?php print_r($tweets->user->name); ?>(@<?php print_r($tweets->user->screen_name);?>)
+                        <a href="<?php print_r($tweets->entities->urls[0]->url) ?>"><?php print_r(date('jS F, Y', strtotime($tweets->created_at))); ?></a>
+                </blockquote>
+            </div>
+            <?php  $count = $count + 1;
+                if ($count%3 == 0) {
+                    ?> </div> <?php
+                }
+        } ?>
     </body>
 </html>
